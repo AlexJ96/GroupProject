3c3c538aec4d2bfc5e3a21627b285ef3ec915dbe
Alex Johnson <alex_football_2k8@hotmail.co.uk>
2016-02-24 01:06:17 +0000

    Basic layout created as an example based on only prototype. Used to show how it'll work and display (example).

--- InterviewBot/src/Test.java
+++ InterviewBot/src/Test.java
@@ -1,30 +1,53 @@
+import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 @SuppressWarnings("unused")
-public class Test extends JFrame {
+public class Test extends JApplet {
 	
-	private static final long serialVersionUID = -2803431175048406077L;
-	private JPanel panel;
+	public static final long serialVersionUID = -2803431175048406077L;
+	public JPanel panel;
 	
 	public static void main(String[] args) {
-		Test test = new Test();
-		test.setVisible(true);
+		init(args);
 	}
 	
-	private Test() {
-		//setUndecorated(true);
-		setSize(800, 500);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//setBackground(new Color(0, 0, 0, 0));
-		panel = new JPanel();
-		panel.setVisible(true);
+	public static void init(String[] args) {
+			Test test = new Test();
+			test.setVisible(true);
+	}
+	
+	public Test() {
 		
-		add(panel);
+		panel = new JPanel();
+		
+		JTextArea textField = new JTextArea();
+		textField.setEditable(true);
+		
+		JTextArea messageField = new JTextArea();
+		messageField.setEditable(true);
+		messageField.setBounds(20, 340, 455, 40);
+		
+		JScrollPane scroll = new JScrollPane(textField);
+	    scroll.setBounds(20, 20, 500, 300);
+	    
+	    JButton clear = new JButton("Clear");
+	    clear.setBounds(480, 340, 40, 20);
+	    
+	    JButton send = new JButton("Send");
+	    send.setBounds(480, 361, 40, 20);
+	    
+	    getContentPane().add(scroll);
+	    getContentPane().add(clear);
+	    getContentPane().add(send);
+	    getContentPane().add(messageField);
+	    getContentPane().add(panel);
+	    
 		setVisible(true);
 	}
 	
